c387e810df4b71de76ee9b1ae48ebb6f6269d7db
Tom <tomshibley@gmail.com>
2021-05-12 15:06:23 -0700

    Updated to work with deployed database

--- server/db/database.js
+++ server/db/database.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize');
-const database = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/products', {logging: false});
+const database = new Sequelize('products', 'postgres', 'sdc', {host: '20.51.213.99', port: '5432', dialect: 'postgres'});
 
 module.exports = database;
